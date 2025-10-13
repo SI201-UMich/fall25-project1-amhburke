20cf83e73f10c4d4e5ad6f994b5db09d476ba1a2
Avery Burke <ab24697@westridge.org>
2025-10-13 10:20:06 -0700

    wrote load_penguins function

--- project1.py
+++ project1.py
@@ -1,1 +1,33 @@
+import csv
 
+def main():
+    """Main function to execute the program logic."""
+
+def load_penguins(csv_file):
+    """Reads csv and creates a list of dictionaries containing species, flipper length, and body mass."""
+    penguins = []
+    with open(csv_file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            penguin = {
+                "species": row["species"],
+                "flipper_length_mm": row["flipper_length_mm"], 
+                "body_mass_g": row["body_mass_g"] 
+            }
+            penguins.append(penguin)
+    return penguins
+
+penguins = load_penguins('penguins.csv')
+print(penguins)
+
+def get_penguin_species(penguins):
+    """Creates a dictionary of only Adelie penguins information"""
+
+def average_flipper_length(adelie_penguins):
+    """Calculates the average flipper length of Adelie penguins"""
+
+def find_above_average(adelie_penguins, average_length, island):
+    """Finds all the Adelie penguins on Biscoe island and returns the percentage that have above average flipper length"""
+
+def results(average, percentage):
+    """Writes the calculated average and percentage aboce average to a text file"""
